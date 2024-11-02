8bea534b9197ab42c35145b62c29ca3c9c3e5074
Artre0n <adorszhan@gmail.com>
2024-11-02 21:34:53 +0300

    #

---
+++ Задание 2, упр 4/упр 4.cs
@@ -0,0 +1,10 @@
+﻿using System;
+
+public class MainClass
+{
+    public static void Main()
+    {
+        int number = int.Parse($"{Console.ReadLine()}") + 10;
+        Console.WriteLine(number);
+    }
+}
