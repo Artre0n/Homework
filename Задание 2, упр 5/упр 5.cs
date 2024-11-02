5d95dbb46fee5e2108f565860896c30fb59f3553
Artre0n <adorszhan@gmail.com>
2024-11-02 22:17:51 +0300

    #

---
+++ Задание 2, упр 5/упр 5.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+public class MainClass
+{
+    public static void Main()
+    {
+        Console.WriteLine($"Введите сторону квадрата");
+        int a = int.Parse($"{Console.ReadLine()}");
+        Console.WriteLine($"Периметр квадрата: {a * 4}");
+    }
+}
---
+++ Задание 2, упр 6/упр 6.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Reflection.Metadata;
+
+public class MainClass
+{
+    public static void Main()
+    {
+        Console.WriteLine($"Введите радиус круга");
+        double r = Convert.ToDouble(Console.ReadLine());
+        double length = 2 * 3.14 * r;
+        double area = 3.14 * r * r;
+        Console.WriteLine($"Длина окружности: {length}, площадь: {area}");
+    }
+}
