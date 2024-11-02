3c2c4d3d6b3cd02eff4c06b60b418b706597b267
Artre0n <adorszhan@gmail.com>
2024-11-02 21:28:49 +0300

    #

---
+++ Задание 2, упражнение 3/упр 3.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+public class MainClass
+{
+    public static void Main()
+    {
+        Console.WriteLine("Введите 4 числа через enter");
+        var x1 = Console.ReadLine();
+        var x2 = Console.ReadLine();
+        var x3 = Console.ReadLine();
+        var x4 = Console.ReadLine();
+        Console.WriteLine();
+        Console.WriteLine(x1);
+        Console.WriteLine(x2);
+        Console.WriteLine(x3);
+        Console.WriteLine(x4);
+    }
+}
