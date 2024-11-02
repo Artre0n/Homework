5249d2219999751886067d27323896c94bffe876
Artre0n <adorszhan@gmail.com>
2024-11-02 19:40:44 +0300

    !

---
+++ Задание 1/Упражнение 2.1.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+public class MainClass
+{
+    public static void Main()
+    {
+        Console.WriteLine("Как вас зовут?");
+        string user_name = Console.ReadLine();
+        Console.WriteLine($"Привет, {user_name}!");
+    }
+}
+
+
