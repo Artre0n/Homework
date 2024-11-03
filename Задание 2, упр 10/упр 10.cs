9f68563c23d50c79eb769948c4d610000acaef41
Artre0n <adorszhan@gmail.com>
2024-11-03 15:17:07 +0300

    #

---
+++ Задание 2, упр 10/упр 10.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+public class MainClass
+{
+    public static void Main()
+    {
+        Console.WriteLine("Мир Труд Май");
+        Console.WriteLine("Мир");
+        Console.WriteLine("\tТруд");
+        Console.WriteLine("\t\tМай");
+    }
+}
