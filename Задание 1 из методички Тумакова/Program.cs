b706ef2e4459c87f108f59e4d89236f374b4c451
Artre0n <adorszhan@gmail.com>
2024-11-04 22:38:23 +0300

    #

---
+++ Задание 1 из методички Тумакова/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+public class MainClass
+{
+    public static void Main()
+    {
+        /*
+        Console.WriteLine("Лабораторная работа упражнение 2.1");
+        Console.WriteLine();
+        Console.WriteLine("Как вас зовут?");
+        string user_name = Console.ReadLine();
+        Console.WriteLine($"Привет, {user_name}!");
+        */
+
+        /*
+        Console.WriteLine("Лабораторная работа упражнение 2.2");
+        Console.WriteLine();
+        Console.WriteLine("Введите два целых числа через enter");
+        int x1 = int.Parse($"{Console.ReadLine()}");
+        int x2 = int.Parse($"{Console.ReadLine()}");
+        try
+        {
+            var division = x1 / x2;
+            Console.WriteLine(division);
+        }
+        catch (Exception)
+        {
+            Console.WriteLine("Деление на 0 недопустимо!");
+        }
+        */
+
+
+        Console.WriteLine("Лабораторная работа д/з 2.2");
+    }   
+}
---
+++ Задание 2 из файла/Program.cs
@@ -0,0 +1,128 @@
+﻿using System;
+
+public class MainClass
+{
+    public static void Main()
+    {
+        /*
+        Console.WriteLine("Упражнение 1");
+        Console.WriteLine();
+        double e = 2.7;
+        Console.WriteLine(e);
+        */
+        /*
+        Console.WriteLine("Упражнение 2");
+        Console.WriteLine();
+        Console.WriteLine("50");
+        Console.WriteLine("10");
+        */
+        /*
+        Console.WriteLine("Упражнение 3");
+        Console.WriteLine();
+        Console.WriteLine("Введите 4 числа через enter");
+        var x1 = Console.ReadLine();
+        var x2 = Console.ReadLine();
+        var x3 = Console.ReadLine();
+        var x4 = Console.ReadLine();
+        Console.WriteLine();
+        Console.WriteLine(x1);
+        Console.WriteLine(x2);
+        Console.WriteLine(x3);
+        Console.WriteLine(x4);
+        */
+        /*
+        Console.WriteLine("Упражнение 4");
+        Console.WriteLine();
+        int number = int.Parse($"{Console.ReadLine()}") + 10;
+        Console.WriteLine(number);
+        */
+        /*
+        Console.WriteLine("Упражнение 5");
+        Console.WriteLine();
+        Console.WriteLine($"Введите сторону квадрата");
+        int a = int.Parse($"{Console.ReadLine()}");
+        Console.WriteLine($"Периметр квадрата: {a * 4}");
+        */
+        /*
+        Console.WriteLine("Упражнение 6");
+        Console.WriteLine();
+        Console.WriteLine($"Введите радиус круга");
+        double r = Convert.ToDouble(Console.ReadLine());
+        double pi = Math.PI;
+        double length = 2 * pi * r;
+        double area = pi * r * r;
+        Console.WriteLine($"Длина окружности: {length}, площадь: {area}");
+        */
+        /*
+        Console.WriteLine("Упражнение 7");
+        Console.WriteLine();
+        double x = Convert.ToDouble(Console.ReadLine());
+        Console.WriteLine(Math.Cos(x));
+        */
+        /*
+        Console.WriteLine("Упражнение 8");
+        Console.WriteLine();
+        int base_1 = int.Parse($"{Console.ReadLine()}");
+        int base_2 = int.Parse($"{Console.ReadLine()}");
+        int h = int.Parse($"{Console.ReadLine()}");
+        var a = Math.Sqrt(Math.Pow((base_1 - base_2 / 2), 2) + h * h);
+        var perimeter = 2 * a + base_1 + base_2;
+        Console.WriteLine(perimeter);
+        */
+        /*
+        Console.WriteLine("Упражнение 9");
+        Console.WriteLine();
+        int cost_candies = 23;
+        int cost_cookies = 200;
+        int cost_apples = 60;
+        Console.WriteLine($"Стоимость одного килограмма конфет: {cost_candies}");
+        Console.WriteLine($"Стоимость одного килограмма печенья: {cost_cookies}");
+        Console.WriteLine($"Стоимость одного килограмма яблок: {cost_apples}");
+
+        Console.WriteLine();
+
+        Console.Write("Введите количество килограмм конфет: ");
+        int kg_candies = int.Parse(Console.ReadLine());
+        Console.Write("Введите количество килограмм печенья: ");
+        int kg_cookies = int.Parse(Console.ReadLine());
+        Console.Write("Введите количество килограмм яблок: ");
+        int kg_apples = int.Parse(Console.ReadLine());
+
+        Console.WriteLine();
+
+        Console.WriteLine($"Стоимость всей покупки: {cost_candies * kg_candies + cost_cookies * kg_cookies + cost_apples * kg_apples}");
+        */
+        /*
+        Console.WriteLine("Упражнение 10");
+        Console.WriteLine();
+        Console.WriteLine("Мир Труд Май");
+        Console.WriteLine("Мир");
+        Console.WriteLine("\tТруд");
+        Console.WriteLine("\t\tМай");
+        */
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    }   
+}
